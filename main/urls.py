acbba683d60c0e192073650c553de84e370588a7
matheeus4568 <rodriguesmatheus4568@gmail.com>
2024-08-04 23:19:10 -0300

    c1

--- main/urls.py
+++ main/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from matheusraquel.views import matheus
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('matheus/',matheus,name='matheus'),
 ]
